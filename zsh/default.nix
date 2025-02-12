ef72a10edd4082b010b2679ac97c94955b6c8f96
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2025-02-11 21:45:00 -0500

    zsh: alias/functions

--- zsh/default.nix
+++ zsh/default.nix
@@ -27,9 +27,16 @@
     ];
 
     shellAliases = rec {
-      gs      = "git status";
       dotfiles = "cd ~/.config/nix";
       railway-local = "RAILWAY_ENV=dev ~/Projects/cli/target/release/railway";
+      ls = "ls --color=auto";
+      gpm = "git pull origin main";
+      gpom = "git pull origin main";
+      gs = "git status";
+      gcm = "git checkout main";
+      gf = "git fetch";
+      gca = "git commit --amend";
+      gpf = "git push -f";
     };
 
     initExtra = ''
@@ -37,6 +44,63 @@
       zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
       eval "$(direnv hook zsh)"
+
+      note() {
+          # Set the EDITOR environment variable to your preferred text editor
+          EDITOR=''${EDITOR:-vi}
+
+          # Set the NOTES_DIR environment variable to the directory where notes should be saved
+          NOTES_DIR=''${NOTES_DIR:-~/notes}
+
+          # Set the file type for notes (so that the editor LSP can work)
+          NOTES_TYPE=''${NOTES_TYPE:-.md}
+
+          # Check if the NOTES_DIR folder exists
+          if [ ! -d "$NOTES_DIR" ]
+          then
+              # If the folder does not exist, create it
+              mkdir "$NOTES_DIR"
+          fi
+
+          if [ -z "$1" ]; then
+              # If no filename is provided, create a new file with a timestamp in the name
+              filename="note-$(date +'%Y-%m-%d_%H-%M-%S')$NOTES_TYPE"
+          else
+              # Use the provided filename
+              filename="$1"
+          fi
+
+          # Open the file in the text editor
+          $EDITOR "$NOTES_DIR/$filename"
+      }
+
+      # notes function
+      notes() {
+          # Set the NOTES_DIR environment variable to the directory where notes should be saved
+          NOTES_DIR=''${NOTES_DIR:-~/notes}
+
+          # Set the internal field separator to a newline character
+          # so that we can loop through the output of 'ls' line by line
+          IFS=$'\n'
+
+          # Print the table header
+          printf "+-------------------------------------+----------------+\n"
+          printf "|                 Note                |      Date      |\n"
+          printf "+-------------------------------------+----------------+\n"
+
+          # Loop through the output of 'ls'
+          ls -tl "$NOTES_DIR" | tail -n +2 | while read -r line; do
+              # Extract the file name and date from the line
+              file=$(echo "$line" | awk '{print $9}')
+              date=$(echo "$line" | awk '{print $6, $7, $8}')
+
+              # Print the file name and date in a table row
+              printf "| %-35s | %-15s|\n" "$file" "$date"
+          done
+
+          # Print the table footer
+          printf "+-------------------------------------+----------------+\n"
+      }
     '';
   };
 }
