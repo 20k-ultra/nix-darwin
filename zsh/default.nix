25f1072ebe89250b65b8f2c06f7174f77d2c2140
20k-ultra <3946250+20k-ultra@users.noreply.github.com>
2025-09-02 20:43:51 -0400

    fix warn

--- zsh/default.nix
+++ zsh/default.nix
@@ -51,85 +51,87 @@
       gpf = "git push -f";
     };
 
-    initExtra = ''
-      # Add Homebrew to PATH for Apple Silicon Macs
-      if [[ -f /opt/homebrew/bin/brew ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      fi
+    initContent = ''
+            # Add Homebrew to PATH for Apple Silicon Macs
+            if [[ -f /opt/homebrew/bin/brew ]]; then
+              eval "$(/opt/homebrew/bin/brew shellenv)"
+            fi
 
-      # Set EDITOR and VISUAL environment variables
-      export EDITOR="nvim"
-      export VISUAL="nvim"
+            # Set EDITOR and VISUAL environment variables
+            export EDITOR="nvim"
+            export VISUAL="nvim"
       
-      # Enable case-insensitive completion
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            # Enable case-insensitive completion
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
-      eval "$(direnv hook zsh)"
+            eval "$(direnv hook zsh)"
 
-      # Set the curl format file path
-      export CURL_TIMED_FORMAT="$HOME/.curl-format"
+            # Set the curl format file path
+            export CURL_TIMED_FORMAT="$HOME/.curl-format"
 
-      # Add the timed_curl function
-      timed_curl() {
-          curl -w "@$CURL_TIMED_FORMAT" -o /dev/null -s "$1"
-      }
+            # Add the timed_curl function
+            timed_curl() {
+                curl -w "@$CURL_TIMED_FORMAT" -o /dev/null -s "$1"
+            }
 
-      note() {
-          # Set the EDITOR environment variable to your preferred text editor
-          EDITOR=''${EDITOR:-vi}
+            note() {
+                # Set the EDITOR environment variable to your preferred text editor
+                EDITOR=''${EDITOR:-vi}
 
-          # Set the NOTES_DIR environment variable to the directory where notes should be saved
-          NOTES_DIR=''${NOTES_DIR:-~/notes}
+                # Set the NOTES_DIR environment variable to the directory where notes should be saved
+                NOTES_DIR=''${NOTES_DIR:-~/notes}
 
-          # Set the file type for notes (so that the editor LSP can work)
-          NOTES_TYPE=''${NOTES_TYPE:-.md}
+                # Set the file type for notes (so that the editor LSP can work)
+                NOTES_TYPE=''${NOTES_TYPE:-.md}
 
-          # Check if the NOTES_DIR folder exists
-          if [ ! -d "$NOTES_DIR" ]
-          then
-              # If the folder does not exist, create it
-              mkdir "$NOTES_DIR"
-          fi
+                # Check if the NOTES_DIR folder exists
+                if [ ! -d "$NOTES_DIR" ]
+                then
+                    # If the folder does not exist, create it
+                    mkdir "$NOTES_DIR"
+                fi
 
-          if [ -z "$1" ]; then
-              # If no filename is provided, create a new file with a timestamp in the name
-              filename="note-$(date +'%Y-%m-%d_%H-%M-%S')$NOTES_TYPE"
-          else
-              # Use the provided filename
-              filename="$1"
-          fi
+                if [ -z "$1" ]; then
+                    # If no filename is provided, create a new file with a timestamp in the name
+                    filename="note-$(date +'%Y-%m-%d_%H-%M-%S')$NOTES_TYPE"
+                else
+                    # Use the provided filename
+                    filename="$1"
+                fi
 
-          # Open the file in the text editor
-          $EDITOR "$NOTES_DIR/$filename"
-      }
+                # Open the file in the text editor
+                $EDITOR "$NOTES_DIR/$filename"
+            }
 
-      # notes function
-      notes() {
-          # Set the NOTES_DIR environment variable to the directory where notes should be saved
-          NOTES_DIR=''${NOTES_DIR:-~/notes}
+            # notes function
+            notes() {
+                # Set the NOTES_DIR environment variable to the directory where notes should be saved
+                NOTES_DIR=''${NOTES_DIR:-~/notes}
 
-          # Set the internal field separator to a newline character
-          # so that we can loop through the output of 'ls' line by line
-          IFS=$'\n'
+                # Set the internal field separator to a newline character
+                # so that we can loop through the output of 'ls' line by line
+                IFS=
+        };
+      }\n'
 
-          # Print the table header
-          printf "+-------------------------------------+----------------+\n"
-          printf "|                 Note                |      Date      |\n"
-          printf "+-------------------------------------+----------------+\n"
+                # Print the table header
+                printf "+-------------------------------------+----------------+\n"
+                printf "|                 Note                |      Date      |\n"
+                printf "+-------------------------------------+----------------+\n"
 
-          # Loop through the output of 'ls'
-          ls -tl "$NOTES_DIR" | tail -n +2 | while read -r line; do
-              # Extract the file name and date from the line
-              file=$(echo "$line" | awk '{print $9}')
-              date=$(echo "$line" | awk '{print $6, $7, $8}')
+                # Loop through the output of 'ls'
+                ls -tl "$NOTES_DIR" | tail -n +2 | while read -r line; do
+                    # Extract the file name and date from the line
+                    file=$(echo "$line" | awk '{print $9}')
+                    date=$(echo "$line" | awk '{print $6, $7, $8}')
 
-              # Print the file name and date in a table row
-              printf "| %-35s | %-15s|\n" "$file" "$date"
-          done
+                    # Print the file name and date in a table row
+                    printf "| %-35s | %-15s|\n" "$file" "$date"
+                done
 
-          # Print the table footer
-          printf "+-------------------------------------+----------------+\n"
-      }
+                # Print the table footer
+                printf "+-------------------------------------+----------------+\n"
+            }
     '';
   };
 }
